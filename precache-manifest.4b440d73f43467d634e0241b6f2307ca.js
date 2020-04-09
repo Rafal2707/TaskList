ad35e9eb50c8df99a2bf5c3a0ad6dc1ec7587750
Rafal Pych <r.pych27@gmail.com>
2020-04-09 16:59:08 +0200

    Updates

---
+++ precache-manifest.4b440d73f43467d634e0241b6f2307ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03bf6ec36d936ae0b4c1ffbfe4491875",
+    "url": "/TaskList/index.html"
+  },
+  {
+    "revision": "208f3fecabba22afb294",
+    "url": "/TaskList/static/css/main.0f8235e6.chunk.css"
+  },
+  {
+    "revision": "068fd0a42ff9bc17a037",
+    "url": "/TaskList/static/js/2.186004cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TaskList/static/js/2.186004cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "208f3fecabba22afb294",
+    "url": "/TaskList/static/js/main.d36babe2.chunk.js"
+  },
+  {
+    "revision": "5d953161c46773ea56df",
+    "url": "/TaskList/static/js/runtime-main.6750c07e.js"
+  }
+]);
